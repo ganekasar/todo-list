b11a41fd9bb585b3b310a757e3c38138682c7449
Ganesh Kasar <ganekasar@gmail.com>
2020-10-14 01:11:54 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,84 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb+srv://admin-ganesh:admin123@cluster0.sfavf.mongodb.net/todolistdb", { useNewUrlParser : true});
+
+const itemsSchema = {
+  name: String
+};
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+  name: "Welcome to your todolist"
+});
+
+const item2 = new Item({
+  name: "This is second list"
+});
+
+const defaultItems = [item1, item2];
+
+app.get("/", function(req, res) {
+
+  Item.find({}, function(err, foundItems) {
+    console.log(foundItems);
+
+    if(foundItems.length == 0) {
+      Item.insertMany(defaultItems, function(err) {
+        if(err) {
+          console.log(err);
+        } else {
+          console.log("Items added to database");
+        }
+      });      
+    }
+
+    res.render("list", {listTitle: "Today", newListItems: foundItems});
+  });
+
+});
+
+app.post("/", function(req, res){
+
+  const itemName = req.body.newItem;
+
+  const item = new Item({
+    name: itemName
+  });
+
+  item.save();
+
+  res.redirect("/");
+
+});
+
+app.post("/delete", function(req, res) {
+  const checkItemID = req.body.checkbox;
+  Item.findByIdAndRemove(checkItemID, function(err){
+    if(err) {
+      console.log(err);
+    }
+  });
+  res.redirect("/");
+});
+
+app.get("/work", function(req,res){
+  res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.get("/about", function(req, res){
+  res.render("about");
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
